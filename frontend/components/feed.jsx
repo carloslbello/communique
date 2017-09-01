e1e27041cd29679b0fb642a712855b978ed0c832
Carlos Liam <carlos@aarzee.me>
2017-09-01 09:39:07 -0400

    Show featured posts if feed is empty

--- frontend/components/feed.jsx
+++ frontend/components/feed.jsx
@@ -5,18 +5,29 @@ class Feed extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { postIds: [] };
+    this.state = { postIds: [], fetched: false };
   }
 
   componentDidMount() {
     this.props.fetchFeedPostIds()
-      .then(res => this.setState({ postIds: res.post_ids }));
+      .then(res => this.setState({ postIds: res.post_ids, fetched: true }));
   }
 
   render() {
+    const noPosts = (this.state.postIds.length === 0 && this.state.fetched);
+    const noPostsInfo = noPosts ? (
+      <div className="landing">
+        <p>Looks like you've got nothing in your feed. You might want to check out these featured posts:</p>
+      </div>
+    ) : null;
+    const postIds = noPosts ? window.landingPostIds : this.state.postIds;
+    debugger;
     return (
-      <div className="posts-info-container">
-        {this.state.postIds.map(postId => <PostInfoContainer key={postId} postId={postId} />)}
+      <div>
+        {noPostsInfo}
+        <div className="posts-info-container">
+          {postIds.map(postId => <PostInfoContainer key={postId} postId={postId} />)}
+        </div>
       </div>
     );
   }
